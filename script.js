3851dea9eb6c4b32d1e525cba95c0818bcef04ff
Sidney <sidney.moretto@gmail.com>
2024-07-26 18:12:07 -0300

    new functions and sections

--- script.js
+++ script.js
@@ -1,209 +1,167 @@
 function selecaoItens() {
-  // Obter valores dos campos
-  const dp = document.getElementById("dp").value;
-  const tipoDisciplina = document.getElementById("tipoDisciplina").value;
+    // Obter valores dos campos
+    const situacaoDisciplina = document.getElementById("situacaoDisciplina").value;
+    const tipoDisciplina = document.getElementById("tipoDisciplina").value;
 
-  /*
- 
-     // Esconder/Mostrar seções de acordo com o tipo de disciplina
-     mostrarSecao('secaoProvas', tipoDisciplina === "teoricaTradicional");
-     mostrarSecao('secaoPims', tipoDisciplina === 'teoricaTecnologica');
-     mostrarSecao('secaoRelatorios', tipoDisciplina === 'praticaLicenciatura');
-     mostrarSecao('secaoLaboratorio', tipoDisciplina === 'praticaLaboratorio');
-     mostrarSecao('secaoAva', tipoDisciplina === 'praticaLaboratorio');
- 
-     // Limpar campos de seções não utilizadas
-     limparSecao('secaoProvas', tipoDisciplina !== 'teoricaTradicional');
-     limparSecao('secaoPims', tipoDisciplina !== 'teoricaTecnologica');
-     limparSecao('secaoRelatorios', tipoDisciplina !== 'praticaLicenciatura');
-     limparSecao('secaoLaboratorio', tipoDisciplina !== 'praticaLaboratorio');
-     
-    */
+    // Limpar campos para não ficar nota calculada errada
+    limparAll();
+    ocultarAll();
 
-  // Limpar campos para não ficar nota calculada errada
-  limparAll();
-  ocultarAll();
-
-  // Definir qual inputs serão inseridos
-  if (dp === "dp") {
+    // Definir qual inputs serão inseridos
     switch (tipoDisciplina) {
-      case "teoricaTradicional":
-        mostrarSecao("secaoProvas", tipoDisciplina === "teoricaTradicional");
-        break;
-      case "teoricaTecnologica":
-        mostrarSecao("secaoProvas", tipoDisciplina === "teoricaTecnologica");
-        break;
+        case "teoricaTradicional":
+            mostrarSecao("secaoProvas", situacaoDisciplina === "regular" || "dp");
+            mostrarSecao("secaoAva", situacaoDisciplina === "regular");
+            mostrarSecao("secaoExame", situacaoDisciplina === "regular" || "dp");
+            break;
+        case "teoricaTecnologica":
+            mostrarSecao("secaoProvas", situacaoDisciplina === "regular" || "dp");
+            mostrarSecao("secaoPims", situacaoDisciplina === "regular");
+            mostrarSecao("secaoAva", situacaoDisciplina === "regular");
+            mostrarSecao("secaoExame", situacaoDisciplina === "regular" || "dp");
+            break;
+        case "praticaLaboratorio":
+            mostrarSecao("secaoProvas", situacaoDisciplina === "regular" || "dp");
+            mostrarSecao("secaoRelatorios", situacaoDisciplina === "regular");
+            mostrarSecao("secaoExame", situacaoDisciplina === "regular" || "dp");
+            break;
+        case "praticaLicenciatura":
+            mostrarSecao("secaoRelatorios", situacaoDisciplina === "regular" || "dp");
+            mostrarSecao("secaoRelatoriosLic", situacaoDisciplina === "regular" || "dp");
+            mostrarSecao("secaoChat", situacaoDisciplina === "regular");
+            mostrarSecao("secaoExame", situacaoDisciplina === "regular" || "dp");
+            break;
+        case "tcc":
+            mostrarSecao("secaoTcc", tipoDisciplina === "tcc");
+            break;
     }
-  } else {
-    // Implementar cálculo para Disciplinas regulares
-    switch (tipoDisciplina) {
-      case "teoricaTradicional":
-        mostrarSecao("secaoProvas", tipoDisciplina === "teoricaTradicional");
-        break;
-      case "teoricaTecnologica":
-        mostrarSecao("secaoProvas", tipoDisciplina === "teoricaTecnologica");
-        break;
-      case "praticaLicenciatura":
-        md = calcularMediaPraticaLicenciatura(
-          relatorio1,
-          relatorio2,
-          relatorioFinal
-        );
-        break;
-      case "praticaLaboratorio":
-        md = calcularMediaPraticaLaboratorio(relatorio1, relatorio2, prova1);
-        break;
-      case "tcc":
-        md = calcularMediaTcc(trabalhoCurso, banca);
-        break;
-      // Implementar cálculo para Estágio e PIM
-    }
-  }
 }
 
 function calcular() {
-  // Obter valores dos campos
-  const anoIngresso = document.getElementById("anoIngresso").value;
-  const dp = document.getElementById("dp").value;
-  const tipoDisciplina = document.getElementById("tipoDisciplina").value;
-  const prova1 = parseFloat(document.getElementById("prova1").value) || 0;
-  const prova2 = parseFloat(document.getElementById("prova2").value) || 0;
-  const prova3 = parseFloat(document.getElementById("prova3").value) || 0;
-  const pim1 = parseFloat(document.getElementById("pim1").value) || 0;
-  const pim2 = parseFloat(document.getElementById("pim2").value) || 0;
-  const relatorio1 =
-    parseFloat(document.getElementById("relatorio1").value) || 0;
-  const relatorio2 =
-    parseFloat(document.getElementById("relatorio2").value) || 0;
-  const relatorioFinal =
-    parseFloat(document.getElementById("relatorioFinal").value) || 0;
-  const exame = parseFloat(document.getElementById("exame").value) || 0;
+    // Obter valores dos campos
+    const anoIngresso = document.getElementById("anoIngresso").value;
+    const situacaoDisciplina = document.getElementById("situacaoDisciplina").value;
+    const tipoDisciplina = document.getElementById("tipoDisciplina").value;
+    const prova1 = parseFloat(document.getElementById("prova1").value) || 0;
+    const pim1 = parseFloat(document.getElementById("pim1").value) || 0;
+    const relatorio1 =
+        parseFloat(document.getElementById("relatorio1").value) || 0;
+    const relatorioFinal =
+        parseFloat(document.getElementById("relatorioFinal").value) || 0;
+    const exame = parseFloat(document.getElementById("exame").value) || 0;
 
-  // Esconder/Mostrar seções de acordo com o tipo de disciplina
-  mostrarSecao("secaoProvas", tipoDisciplina === "teoricaTradicional");
-  mostrarSecao("secaoPims", tipoDisciplina === "teoricaTecnologica");
-  mostrarSecao("secaoRelatorios", tipoDisciplina === "praticaLicenciatura");
-  mostrarSecao("secaoLaboratorio", tipoDisciplina === "praticaLaboratorio");
+    // Calcular média da disciplina (MD)
+    let md = 0;
+    switch (tipoDisciplina) {
+        case "teoricaTradicional":
+            md = calcularMediaTeoricaTradicional(prova1, prova2, prova3);
+            break;
+        case "teoricaTecnologica":
+            md = calcularMediaTeoricaTecnologica(prova1, prova2, prova3, pim1, pim2);
+            break;
+        case "praticaLicenciatura":
+            md = calcularMediaPraticaLicenciatura(
+                relatorio1,
+                relatorio2,
+                relatorioFinal
+            );
+            break;
+        case "praticaLaboratorio":
+            md = calcularMediaPraticaLaboratorio(relatorio1, relatorio2, prova1);
+            break;
+        case "tcc":
+            md = calcularMediaTcc(trabalhoCurso, banca);
+            break;
+    }
 
-  // Limpar campos de seções não utilizadas
-  limparSecao("secaoProvas", tipoDisciplina !== "teoricaTradicional");
-  limparSecao("secaoPims", tipoDisciplina !== "teoricaTecnologica");
-  limparSecao("secaoRelatorios", tipoDisciplina !== "praticaLicenciatura");
-  limparSecao("secaoLaboratorio", tipoDisciplina !== "praticaLaboratorio");
+    // Arredondamento da MD
+    md = arredondarMedia(md, anoIngresso);
 
-  // Calcular média da disciplina (MD)
-  let md = 0;
-  switch (tipoDisciplina) {
-    case "teoricaTradicional":
-      md = calcularMediaTeoricaTradicional(prova1, prova2, prova3);
-      break;
-    case "teoricaTecnologica":
-      md = calcularMediaTeoricaTecnologica(prova1, prova2, prova3, pim1, pim2);
-      break;
-    case "praticaLicenciatura":
-      md = calcularMediaPraticaLicenciatura(
-        relatorio1,
-        relatorio2,
-        relatorioFinal
-      );
-      break;
-    case "praticaLaboratorio":
-      md = calcularMediaPraticaLaboratorio(relatorio1, relatorio2, prova1);
-      break;
-    case "tcc":
-      md = calcularMediaTcc(trabalhoCurso, banca);
-      break;
-    // Implementar cálculo para Estágio e PIM
-  }
+    // Calcular média final (MF)
+    let mf = 0;
+    if (md < (anoIngresso === "2022" ? 6 : 7)) {
+        mf = calcularMediaFinalComExame(md, exame);
+    } else {
+        mf = md;
+    }
 
-  // Arredondamento da MD
-  md = arredondarMedia(md, anoIngresso);
+    // Arredondamento da MF
+    mf = arredondarMedia(mf, anoIngresso);
 
-  // Calcular média final (MF)
-  let mf = 0;
-  if (md < (anoIngresso === "2022" ? 6 : 7)) {
-    mf = calcularMediaFinalComExame(md, exame);
-  } else {
-    mf = md;
-  }
+    // Exibir resultados
+    document.getElementById("md").textContent = md.toFixed(2);
+    document.getElementById("mf").textContent = mf.toFixed(2);
 
-  // Arredondamento da MF
-  mf = arredondarMedia(mf, anoIngresso);
-
-  // Exibir resultados
-  document.getElementById("md").textContent = md.toFixed(1);
-  document.getElementById("mf").textContent = mf.toFixed(1);
-
-  if (mf >= (anoIngresso === "2022" ? 5 : 5)) {
-    alert("Aprovado!");
-  } else {
-    alert("Reprovado!");
-  }
+    if (mf >= (anoIngresso === "2022" ? 5 : 5)) {
+        alert("Aprovado!");
+    } else {
+        alert("Reprovado!");
+    }
 }
 
 function mostrarSecao(secaoId, mostrar) {
-  const secao = document.getElementById(secaoId);
-  secao.style.display = mostrar ? "block" : "none";
+    const secao = document.getElementById(secaoId);
+    secao.style.display = mostrar ? "block" : "none";
 }
 
 function limparSecao(secaoId, limpar) {
-  if (limpar) {
-    const inputs = document
-      .getElementById(secaoId)
-      .querySelectorAll('input[type="number"]');
-    for (const input of inputs) {
-      input.value = "";
+    if (limpar) {
+        const inputs = document
+            .getElementById(secaoId)
+            .querySelectorAll('input[type="number"]');
+        for (const input of inputs) {
+            input.value = "";
+        }
     }
-  }
 }
 
 function limparAll() {
-  const inputs = document.querySelectorAll('input[type="number"]');
-  for (const input of inputs) {
-    input.value = "";
-  }
+    const inputs = document.querySelectorAll('input[type="number"]');
+    for (const input of inputs) {
+        input.value = "";
+    }
 }
 
 function ocultarAll() {
-  const sections = document.getElementsByTagName("section");
-  for (const section of sections) {
-    section.style.display = "none";
-  }
+    const sections = document.getElementsByTagName("section");
+    for (const section of sections) {
+        section.style.display = "none";
+    }
 }
 
 function calcularMediaTeoricaTradicional(prova1, prova2, prova3) {
-  return (9 * prova1 + prova2 + prova3) / 10;
+    return (9 * prova1 + prova2 + prova3) / 10;
 }
 
 function calcularMediaTeoricaTecnologica(prova1, prova2, prova3, pim1, pim2) {
-  return (7 * (prova1 + prova2 + prova3) + 2 * (pim1 + pim2)) / 10;
+    return (7 * (prova1 + prova2 + prova3) + 2 * (pim1 + pim2)) / 10;
 }
 
 function calcularMediaPraticaLicenciatura(
-  relatorio1,
-  relatorio2,
-  relatorioFinal
+    relatorio1,
+    relatorio2,
+    relatorioFinal
 ) {
-  return (2 * (relatorio1 + relatorio2) + 7 * relatorioFinal) / 10;
+    return (2 * (relatorio1 + relatorio2) + 7 * relatorioFinal) / 10;
 }
 
 function calcularMediaPraticaLaboratorio(relatorio1, relatorio2, prova1) {
-  return (3 * (relatorio1 + relatorio2) + 7 * prova1) / 10;
+    return (3 * (relatorio1 + relatorio2) + 7 * prova1) / 10;
 }
 
 function calcularMediaTcc(trabalhoCurso, banca) {
-  return (7 * trabalhoCurso + 3 * banca) / 10;
+    return (7 * trabalhoCurso + 3 * banca) / 10;
 }
 
-function calcularMediaFinalComExame(md, exame) {
-  return (md + exame) / 2;
+function calcularMediaFinalComExame(md, exame,) {
+    return (md + exame) / 2;
 }
 
 function arredondarMedia(media, anoIngresso) {
-  if (anoIngresso === "2022" && media >= 5.7 && media < 6) {
-    media = 6;
-  } else if (media >= 6.7 && media < 7) {
-    media = 7;
-  }
-  return media;
+    if (anoIngresso === "2022" && media >= 5.7 && media < 6) {
+        media = 6;
+    } else if (media >= 6.7 && media < 7) {
+        media = 7;
+    }
+    return media;
 }
